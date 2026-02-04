85007a6922978ee1a366e1baf05ff9c0cec1f16e
Raaaka <bigboss.sk75@gmail.com>
2026-02-04 23:32:06 +0530

    no speed to 500

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ proceedBtn.addEventListener("click", () => {
    NO BUTTON (FIXED)
 ========================= */
 let noInterval = null;
-let noSpeed = 650;
+let noSpeed = 500;
 let noClickCount = 0;
 
 const sarcasticReplies = [
